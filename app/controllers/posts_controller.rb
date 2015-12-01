ab80ebc3eba9aa84a0588d292ff18696072ce5aa
Julia Baritz <jbaritz@gmail.com>
2015-11-30 22:09:55 -0600

    home page redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
 
   def home_feed
-
+    if !current_user
+      redirect_to '/users/sign_in'
+    end   
   end
+
 end
