7c39cb3fa7b95f2b1b677fccff3103e79917d2ff
Julia Baritz <jbaritz@gmail.com>
2016-01-12 22:14:37 -0600

    send post request for following user

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,10 +3,14 @@ class FollowsController < ApplicationController
   def user_create
     u = User.find_by(username: params[:username])
     f = Follow.new(follower_id: current_user.id, followed_id: u['id'], followed_type: 'User')
+    f.save
+    redirect_to '/' + params[:username]
   end
 
   def comm_create
     c = Community.find_by(name: params[:name])
     f = Follow.new(follower_id: current_user.id, followed_id: c['id'], followed_type: 'Community')
+    f.save
+    redirect_to '/community/' + params[:name]
   end
 end
